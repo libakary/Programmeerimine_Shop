24ee97f6a15a2c0cb4f4a717529492fcf451e1cc
Karol <libakary@users.noreply.github.com>
2022-11-24 09:21:07 +0200

    DomainModels

---
+++ TARge21Shop/TARge21Shop.Core/Domain/Spaceship/Spaceship.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TARge21Shop.Core.Domain.Spaceship
+{
+    public class Spaceship
+    {
+        public Guid? Id { get; set; } // guid parem kasutada, seal rohkem ruumi ja kombinatsioone
+    }
+}
