f5f5b46928471ee05035e7176e1cbfad8babfeb7
Karol <libakary@users.noreply.github.com>
2022-12-01 11:10:16 +0200

    Controller ja Index

---
+++ TARge21Shop/TARge21Shop/Controllers/SpaceshipsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TARge21Shop.Data;
+using TARge21Shop.Models.Spaceship;
+
+namespace TARge21Shop.Controllers
+{
+    public class SpaceshipsController : Controller
+    {
+        private readonly TARge21ShopContext _context;
+        public SpaceshipsController (
+            TARge21ShopContext context
+            )
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var result = _context.Spaceships
+                .OrderByDescending(y => y.CreatedAt)
+                .Select(x => new SpaceshipIndexViewModel
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Type = x.Type,
+                    Passengers = x.Passengers,
+                    EnginePower = x.EnginePower
+                });
+            return View(result);
+        }
+    }
+}
