740629c73b40009a9ef3068fb7ea1b48e1fa6acf
Karol <libakary@users.noreply.github.com>
2023-02-23 12:12:15 +0200

    Adding weather stuff

---
+++ TARge21Shop/TARge21Shop.Core/Dto/WeatherDtos/WeatherRootDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TARge21Shop.Core.Dto.WeatherDtos
+{
+    public class WeatherRootDto
+    {
+        public HeadlineDto Headline { get; set; }
+        public List<DailyForecastsDto> DailyForecasts { get; set; }
+		public DateTime EffectiveDate { get; set; }
+	}
+}
