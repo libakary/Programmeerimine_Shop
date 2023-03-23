7e3858d09965da22429be992df59eeb0fabc9f1e
Karol <libakary@users.noreply.github.com>
2023-03-23 17:42:18 +0200

    tests

--- TARge21Shop/TARge21Shop.SpaceshipTest/SpaceshipTest.cs
+++ TARge21Shop/TARge21Shop.SpaceshipTest/SpaceshipTest.cs
@@ -8,6 +8,7 @@ using TARge21Shop.Core.ServiceInterface;
 using Xunit;
 using TARge21Shop.SpaceshipTest;
 using TARge21Shop.Core.Domain;
+using System.Xml.Linq;
 
 namespace TARge21Shop.SpaceshipTest
 {
@@ -20,7 +21,7 @@ namespace TARge21Shop.SpaceshipTest
 
 			SpaceshipDto spaceship = new SpaceshipDto();
 
-			spaceship.Id = Guid.Parse(guid);
+			//spaceship.Id = Guid.Parse(guid);
 			spaceship.Name = "asd";
 			spaceship.Type = "asd";
 			spaceship.Crew = 123;
@@ -77,31 +78,107 @@ namespace TARge21Shop.SpaceshipTest
 		public async Task Should_DeleteByIdSpaceship_WhenDeleteSpaceship()
 		{
 			// arrange
-			//Guid guid = Guid.NewGuid();
-			Guid guid = Guid.Parse("2d3d37d1-9ba4-4fd3-94d8-cb249420a069");
-
-			SpaceshipDto spaceship = new SpaceshipDto();
-
-			spaceship.Id = Guid.Parse("2d3d37d1-9ba4-4fd3-94d8-cb249420a069");
-			spaceship.Name = "asd";
-			spaceship.Type = "asd";
-			spaceship.Crew = 123;
-			spaceship.Passengers = 123;
-			spaceship.CargoWeight = 123;
-			spaceship.FullTripsCount = 123;
-			spaceship.MaintenanceCount = 1000;
-			spaceship.LastMaintenance = DateTime.Now;
-			spaceship.EnginePower = 1000;
-			spaceship.MaidenLaunch = DateTime.Now;
-			spaceship.BuiltDate = DateTime.Now;
-			spaceship.CreatedAt = DateTime.Now;
-			spaceship.ModifiedAt = DateTime.Now;
+			SpaceshipDto spaceship = MockSpaceshipData();
+			var addSpaceship = await Svc<ISpaceshipsServices>().Create(spaceship);
 
 			//act
-			await Svc<ISpaceshipsServices>().Delete(guid);
+			var result = await Svc<ISpaceshipsServices>().Delete((Guid)addSpaceship.Id);
 
 			//assert
-			Assert.Equal(guid, spaceship.Id);
+			Assert.Equal(result, addSpaceship);
+		}
+
+		[Fact]
+		private async Task Should_UpdateSpaceship_WhenUpdateData()
+		{
+			var guid = new Guid("2d3d37d1-9ba4-4fd3-94d8-cb249420a069");
+
+			Spaceship spaceship = new Spaceship();
+
+			SpaceshipDto dto = MockSpaceshipData();
+
+			spaceship.Id = Guid.Parse("2d3d37d1-9ba4-4fd3-94d8-cb249420a069");
+			spaceship.Name = "Name123";
+			spaceship.Type = "asd";
+			spaceship.Crew = 123123;
+			spaceship.Passengers = 123123;
+			spaceship.CargoWeight = 123123;
+			spaceship.FullTripsCount = 123123;
+			spaceship.MaintenanceCount = 1000123;
+			spaceship.LastMaintenance = DateTime.Now.AddYears(1);
+			spaceship.EnginePower = 10123;
+			spaceship.MaidenLaunch = DateTime.Now.AddYears(1);
+			spaceship.BuiltDate = DateTime.Now.AddYears(1);
+			spaceship.CreatedAt = DateTime.Now.AddYears(1);
+			spaceship.ModifiedAt = DateTime.Now.AddYears(1);
+
+			await Svc<ISpaceshipsServices>().Update(dto);
+
+			Assert.Equal(spaceship.Id, guid);
+			Assert.DoesNotMatch(spaceship.Name, dto.Name);
+			Assert.DoesNotMatch(spaceship.EnginePower.ToString(), dto.EnginePower.ToString());
+			Assert.Equal(spaceship.Crew, dto.Crew);
+		}
+
+		[Fact]
+		private async Task Should_UpdateSpaceship_WhenUpdateDataVersion2()
+		{
+			SpaceshipDto dto = MockSpaceshipData();
+			var createSpaceship = await Svc<ISpaceshipsServices>().Create(dto);
+
+			SpaceshipDto update = MockUpdateSpaceship();
+			var result = await Svc<ISpaceshipsServices>().Update(update);
+
+			Assert.Equal(update.Id, dto.Id);
+			Assert.DoesNotMatch(result.Name, createSpaceship.Name);
+			Assert.DoesNotMatch(result.EnginePower.ToString(), createSpaceship.EnginePower.ToString());
+			Assert.Equal(result.Crew, createSpaceship.Crew);
+			Assert.NotEqual(result.ModifiedAt, createSpaceship.ModifiedAt);
+		}
+
+		private SpaceshipDto MockSpaceshipData()
+		{
+			SpaceshipDto spaceship = new()
+			{
+				Name = "Name",
+				Type = "asd",
+				Crew = 123,
+				Passengers = 123,
+				CargoWeight = 123,
+				FullTripsCount = 123,
+				MaintenanceCount = 1000,
+				LastMaintenance = DateTime.Now,
+				EnginePower = 1000,
+				MaidenLaunch = DateTime.Now,
+				BuiltDate = DateTime.Now,
+				CreatedAt = DateTime.Now,
+				ModifiedAt = DateTime.Now,
+			};
+
+			return spaceship;
+		}
+		private SpaceshipDto MockUpdateSpaceship()
+		{
+			SpaceshipDto update = new()
+			{
+				Name = "Name123",
+				Type = "asd",
+				Crew = 123,
+				Passengers = 123123,
+				CargoWeight = 123123,
+				FullTripsCount = 123123,
+				MaintenanceCount = 1000123,
+				LastMaintenance = DateTime.Now.AddYears(1),
+				EnginePower = 10123,
+				MaidenLaunch = DateTime.Now.AddYears(1),
+				BuiltDate = DateTime.Now.AddYears(1),
+				CreatedAt = DateTime.Now.AddYears(1),
+				ModifiedAt = DateTime.Now.AddYears(1),
+
+			};
+
+			return update;
+
 		}
 	}
 }
